5f983288902041ce75c6dc72da369624c4b1447c
Joshua Morse <joshua.morse@iostudio.com>
2011-12-05 14:38:18 -0600

    init

---
+++ Repository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace KnpLabs;
+
+use Doctrine\DBAL\Connection;
+
+/**
+ * Represents a base Repository.
+ * 
+ * @abstract
+ * @package 
+ * @version $id$
+ */
+abstract class Repository
+{
+    abstract public function getTableName();
+
+    public $db;
+
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
+
+    public function insert(array $data)
+    {
+        return $this->db->insert($this->getTableName(), $data);
+    }
+
+    public function update(array $data, array $identifier)
+    {
+        return $this->db->update($this->getTableName(), $data, $identifier);
+    }
+
+    public function delete(array $identifier)
+    {
+        return $this->db->delete($this->getTableName(), $identifier);
+    }
+
+    /**
+     * Returns a record by supplied id.
+     * 
+     * @param mixed $id 
+     * @access public
+     * @return array
+     */
+    public function find($id)
+    {
+        return $this->db->fetchAssoc(sprintf('SELECT * FROM %s WHERE id = ? LIMIT 1', $this->getTableName()), array((int) $id));
+    }
+
+    /**
+     * Returns all records from this repository's table. 
+     * 
+     * @access public
+     * @return array
+     */
+    public function findAll()
+    {
+        return $this->db->fetchAll(sprintf('SELECT * FROM %s', $this->getTableName()));
+    }
+}
