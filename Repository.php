6497441ada3a58481a8a31aeba29e5b45e1ae807
Geoffrey Bachelet <grosfrais@gmail.com>
2011-12-06 21:26:02 +0100

    added a few docblocks

--- Repository.php
+++ Repository.php
@@ -6,42 +6,65 @@ use Doctrine\DBAL\Connection;
 
 /**
  * Represents a base Repository.
- * 
- * @abstract
- * @package 
- * @version $id$
  */
 abstract class Repository
 {
+    /**
+     * @return string
+     */
     abstract public function getTableName();
 
+    /**
+     * @var Doctrine\DBAL\Connection
+     */
     public $db;
 
+    /**
+     * @param Doctrine\DBAL\Connection $db
+     */
     public function __construct(Connection $db)
     {
         $this->db = $db;
     }
 
+    /**
+     * Inserts a table row with specified data.
+     *
+     * @param array $data An associative array containing column-value pairs.
+     * @return integer The number of affected rows.
+     */
     public function insert(array $data)
     {
         return $this->db->insert($this->getTableName(), $data);
     }
 
+    /**
+     * Executes an SQL UPDATE statement on a table.
+     *
+     * @param array $identifier The update criteria. An associative array containing column-value pairs.
+     * @param array $types Types of the merged $data and $identifier arrays in that order.
+     * @return integer The number of affected rows.
+     */
     public function update(array $data, array $identifier)
     {
         return $this->db->update($this->getTableName(), $data, $identifier);
     }
 
+    /**
+     * Executes an SQL DELETE statement on a table.
+     *
+     * @param array $identifier The deletion criteria. An associateve array containing column-value pairs.
+     * @return integer The number of affected rows.
+     */
     public function delete(array $identifier)
     {
         return $this->db->delete($this->getTableName(), $identifier);
     }
 
     /**
-     * Returns a record by supplied id.
+     * Returns a record by supplied id
      * 
      * @param mixed $id 
-     * @access public
      * @return array
      */
     public function find($id)
@@ -50,9 +73,8 @@ abstract class Repository
     }
 
     /**
-     * Returns all records from this repository's table. 
+     * Returns all records from this repository's table
      * 
-     * @access public
      * @return array
      */
     public function findAll()
